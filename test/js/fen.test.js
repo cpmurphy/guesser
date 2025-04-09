5ceaa3ae4c93f140efa38242a7e62e07b007e4ec
Christian Murphy <cpmmu@yahoo.com>
2025-04-09 18:55:11 -0500

    Fix Fen tests

--- test/js/fen.test.js
+++ test/js/fen.test.js
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import Fen from '../../public/scripts/fen.js';
 
 describe('Fen', () => {
@@ -5,13 +6,13 @@ describe('Fen', () => {
     it('converts an empty board to the correct FEN string', () => {
       const position = {};
       const fen = Fen.objToFen(position);
-      expect(fen).toBe('7/8/8/8/8/8/8/8');
+      expect(fen).toBe('8/8/8/8/8/8/8/8');
     });
 
     it('converts a position with two kings to the correct FEN string', () => {
-      const position = { 'e0': 'wk', 'e8': 'bk' };
+      const position = { 'e1': 'wk', 'e8': 'bk' };
       const fen = Fen.objToFen(position);
-      expect(fen).toBe('3k3/8/8/8/8/8/8/4K3')
+      expect(fen).toBe('4k3/8/8/8/8/8/8/4K3')
     });
   });
 });

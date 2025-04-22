e71ac7dbac9ef508bd32e8b5118cb87ee5cbcf63
Christian Murphy <cpmmu@yahoo.com>
2025-04-22 18:07:07 -0500

    Add JS unit test coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, coverageConfigDefaults } from 'vitest/config'
 import path from 'path'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      reportsDirectory: './coverage',
+      exclude: [
+	...coverageConfigDefaults.exclude,
+        'node_modules/**',
+        'sandbox/**',
+        'test/**',
+        '**/*.d.ts',
+        '**/vendor/**',
+        '**/3rdparty/**'
+      ]
+    }
   },
   resolve: {
     alias: {
